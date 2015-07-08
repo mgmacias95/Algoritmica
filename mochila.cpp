f36997b135f54bbd7548691ed83f00aff02142c0
Marta Gómez Macías <mgmacias95@correo.ugr.es>
2015-07-08 12:22:46 +0200

    toques finales

--- mochila.cpp
+++ mochila.cpp
@@ -1,42 +1,48 @@
 #include <iostream>
 #include <list>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 struct Objeto {
     int beneficio;
     int peso;
+    float benporpeso;
     bool operator < (Objeto & obj1) {
-        return ((beneficio/peso) < (obj1.beneficio/obj1.peso));
+        return (benporpeso > obj1.benporpeso);
     }
-    bool operator > (Objeto & obj1) {
-        return ((beneficio/peso) > (obj1.beneficio/obj1.peso));
+    friend ostream & operator<< (ostream & os, const Objeto & o) {
+        os << "Peso: " << o.peso << '\t' << "Beneficio: " << o.beneficio << 
+            '(' << o.benporpeso << ')';
+        return os;
     }
     Objeto (int b, int p) {
         beneficio = b;
         peso = p;
+        benporpeso = (float)beneficio/peso;
     }
 };
 
 list<float> Mochila (int lim_peso, list<Objeto> & objetos) {
     list<float> sol;
     int peso_actual = 0;
-    list<Objeto>::reverse_iterator rit;
+    list<Objeto>::iterator rit;
 
-    for (rit=objetos.rbegin(); rit!=objetos.rend() && lim_peso > peso_actual; ++rit) {
-        if (peso_actual <= (*rit).peso) {
+    for (rit=objetos.begin(); rit!=objetos.end() && lim_peso > peso_actual; ++rit) {
+        if ((peso_actual + (*rit).peso) <= lim_peso) {
             sol.push_back(1);
             peso_actual += (*rit).peso;
         }
 
         else {
-            sol.push_back((lim_peso-peso_actual)/((*rit).peso));
+            float ben = (float)(lim_peso-peso_actual)/((*rit).peso);
+            sol.push_back(ben);
             peso_actual = lim_peso;
         }
     }
 
-    while (rit != objetos.rend()){
+    while (rit != objetos.end()){
         sol.push_back(0);
         ++rit;
     }
@@ -44,12 +50,12 @@ list<float> Mochila (int lim_peso, list<Objeto> & objetos) {
     return sol;
 }
 
-int CalculaBeneficioTotal (list<float> & m, list<Objeto> & objetos) {
-    int ben = 0;
+float CalculaBeneficioTotal (list<float> & m, list<Objeto> & objetos) {
+    float ben = 0;
     list<float>::iterator it_f;
-    list<Objeto>::reverse_iterator rit_o;
-    for (it_f = m.begin(), rit_o = objetos.rbegin(); it_f != m.end() && 
-        rit_o != objetos.rend(); ++it_f, ++rit_o)
+    list<Objeto>::iterator rit_o;
+    for (it_f = m.begin(), rit_o = objetos.begin(); it_f != m.end() && 
+        rit_o != objetos.end(); ++it_f, ++rit_o)
         ben += (*it_f) * (*rit_o).beneficio; 
 
     return ben;
@@ -74,26 +80,34 @@ int main(int argc, char const *argv[]) {
         candidatos.push_back(obj);
     }
 
+    clock_t antes, despues;
+
+    antes = clock();
     candidatos.sort();
 
-    cout << "Los objetos de la mochila ordenados de mayor a menor son:" << endl;
-    list<Objeto>::reverse_iterator rit;
-    for (rit=candidatos.rbegin(); rit!=candidatos.rend(); ++rit)
-        cout << (*rit).beneficio << " " << (*rit).peso << "\t";
-    cout << endl;
+    // cout << "Los objetos de la mochila ordenados de mayor a menor son:" << endl;
+    // list<Objeto>::iterator rit;
+    // for (rit=candidatos.begin(); rit!=candidatos.end(); ++rit)
+    //     cout << (*rit).beneficio << " " << (*rit).peso << "\t";
+    // cout << endl;
 
     int pesomochi = atoi(argv[argc-1]);
 
     list<float> m = Mochila(pesomochi, candidatos);
+    despues = clock();
 
     cout << "La proporcion de cada uno que cogemos es:" << endl;
-    list<float>::iterator it;
-    for (it=m.begin(); it!=m.end(); ++it)
-        cout << *it << "\t";
-    cout << endl;
+    list<float>::iterator it; list<Objeto>::iterator rit = candidatos.begin();
+    for (it=m.begin(); it!=m.end(); ++it) {
+        cout << *rit << " -> " <<  *it << endl;
+        ++rit;
+    }
+
 
     cout << "El beneficio total es, por tanto " <<
         CalculaBeneficioTotal(m, candidatos) << endl;
+    
+    cout << "Tiempo: " << (despues-antes)/(double)CLOCKS_PER_SEC << endl;
 
     return 0;
 }
